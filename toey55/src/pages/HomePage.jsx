0ea7cc0ea61339d853fee40831e333b4ae6044a0
toey-jsd9 <toeykenji@gmail.com>
2025-05-06 16:04:38 +0700

    toey fix

--- toey55/src/pages/HomePage.jsx
+++ toey55/src/pages/HomePage.jsx
@@ -14,13 +14,7 @@ const HomePage = () => {
       <h1 className="text-4xl font-bold mb-4 text-blue-600">
         📒 Welcome to Notes App!
       </h1>
-      <p className="text-gray-600">
-        Please{" "}
-        <a href="/login" className="text-blue-500 hover:underline">
-          Login
-        </a>{" "}
-        to continue.
-      </p>
+      
     </div>
   );
 };
